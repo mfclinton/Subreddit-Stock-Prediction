e9e09c1536f28840aa27fe00b969f90f57e82fa8
akaysh <ksh.shrm1@gmail.com>
2021-05-09 16:10:21 -0400

    Sentiment Vectors from vader for prediction

--- code/data_prediction/train.py
+++ code/data_prediction/train.py
@@ -12,11 +12,11 @@ def train(data):
     # Data Processing
     features = data.to_numpy()[:,:-1]
     labels = data.to_numpy()[:,-1]
-    print("Goes Up Percentage = ", labels.sum() / labels.shape[0])
     num_features = features.shape[1]
 
-    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, stratify=labels)
 
+    print("Goes Up Percentage = ", y_test.sum() / y_test.shape[0])
     X_train = torch.from_numpy(X_train).cuda()
     X_test = torch.from_numpy(X_test).cuda()
     y_train = torch.from_numpy(y_train).cuda()
@@ -25,16 +25,16 @@ def train(data):
     train_dataset = TensorDataset(X_train, y_train)
     # test_dataset = TensorDataset(X_test, y_test)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=10, shuffle=True, drop_last=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=20, shuffle=True)
     # test_dataloader = DataLoader(train_dataset, batch_size=20, shuffle=True)
 
-    model = Prelim_Model(num_features, 64, 1).cuda()
+    model = Prelim_Model(num_features, 20, 1).cuda()
     model.train()
 
-    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
     loss_function = nn.BCELoss()
 
-    num_epochs = 20
+    num_epochs = 10
     for epoch in range(num_epochs):
         total_loss = 0
         total_accuracy = 0
@@ -45,21 +45,19 @@ def train(data):
             outputs = model(inputs).squeeze()
             predicted_labels = torch.round(outputs)
             
-            # print(outputs.size(), labels.size())
-            # print(inputs, outputs)
             loss = loss_function(outputs, labels)
             loss.backward()
             optimizer.step()
 
             total_loss += loss
-            total_accuracy += (predicted_labels == labels).sum() / labels.size()[0]
+            total_accuracy += torch.true_divide((predicted_labels == labels).sum() , labels.size()[0])
             # print("Loss : ", loss)
-        # print("Avg Loss : ", total_loss / i)
-        # print("Avg Accuracy : ", total_accuracy / i)
+        print("Avg Loss : ", total_loss / i)
+        print("Avg Accuracy : ", total_accuracy / i)
 
     outputs = model(X_test).squeeze()
     predicted_labels = torch.round(outputs)
-    accuracy = (predicted_labels == y_test).sum() / y_test.size()[0]
+    accuracy = torch.true_divide((predicted_labels == y_test).sum() , y_test.size()[0])
     print("Test Accuracy : ", accuracy)
 
 
@@ -75,5 +73,4 @@ if __name__ == "__main__":
             continue
         print(f"--------------{filename}-------------")
         data = pd.read_csv(f"data/{filename}")
-        # print(data)
         train(data)
